5ec50e8a7e3222b94556fd5ff37e07f4b03e4859
Jason Kerr <jkerr013@gmail.com>
2019-05-17 14:24:13 -0400

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'aws-graphql',
+    script: './app.js',
+    node_args: 'dotenv/config'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-3-14-224-126.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/aws-test.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:pixeltight/max-graphql.git',
+      path: '/home/ubuntu/max-graphql',
+      'post-deploy': 'pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
