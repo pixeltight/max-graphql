5aefbf385b09dd8abfb48ff4afca026bfa0d7fb2
Jason Kerr <jkerr013@gmail.com>
2019-05-20 13:53:35 -0400

    test variable

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const test = 'test'
 const app = express()
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
-app.listen(8000, () => console.log('Server running on port 8000'))
+app.listen(8000, () => console.log('Server running on port 8000 ', test))
