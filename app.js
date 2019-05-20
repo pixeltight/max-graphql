f529759bab412a7a41f4f78340c212601f6dfb9b
Jason Kerr <jkerr013@gmail.com>
2019-05-20 14:12:42 -0400

    refresh app.js, add frontend to .gitignore

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
+require('dotenv').config()
 const express = require('express')
-const test = 'test'
+const bodyParser = require('body-parser')
+const grahpqlHttp = require('express-graphql')
+const mongoose = require('mongoose')
+const isAuth = require('./middleware/is-auth')
+
+const graphqlSchema = require('./graphql/schema/index')
+const graphqlResolvers = require('./graphql/resolvers/index')
+
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
+
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
 })
-app.listen(8000, () => console.log('Server running on port 8000 ', test))
+
+app.use(isAuth)
+
+app.use('/graphql', grahpqlHttp({
+  schema: graphqlSchema,
+  rootValue: graphqlResolvers,
+  graphiql: true
+}))
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-2kcm0.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(8000)
+  })
+  .catch(err => {
+    console.log(err)
+  })
